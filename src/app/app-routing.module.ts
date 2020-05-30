fc193367a125bb9b19a8bf7f0bbb731a4b0cc272
Nameyho <37048681+Nameyho@users.noreply.github.com>
2020-05-30 19:18:19 +0200

    30 mai
    petite modification dans le menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,29 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import {CoursComponent} from './cours/cours.component';
 import {CoursDetailsComponent} from './cours-details/cours-details.component';
+import {ElevesComponent} from './eleves/eleves.component';
+import {MessagesComponent} from './messages/messages.component';
+import {AbsencesComponent} from './absences/absences.component';
 
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'cours', component: CoursComponent},
-  {path: 'details/:id', component: CoursDetailsComponent}
+  {path: 'home', component: HomeComponent,
+  children: [
+    {path: 'cours', component: CoursComponent}
+  ]},
+  {path: 'details/:id', component: CoursDetailsComponent},
+  {path : 'eleves', component : ElevesComponent},
+  {path : 'messages', component : MessagesComponent},
+  {path : 'absences', component : AbsencesComponent},
+
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import {RestapiService} from './restapi.service';
 import {AppRoutingModule} from './app-routing.module';
 import {FormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {BasicAuthInterceptService} from './service/basic-auth-intercept.service';
 import { CoursComponent } from './cours/cours.component';
 import { CoursDetailsComponent } from './cours-details/cours-details.component';
-
+import { ElevesComponent } from './eleves/eleves.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
+import { AbsencesComponent } from './absences/absences.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -19,14 +23,22 @@ import { CoursDetailsComponent } from './cours-details/cours-details.component';
     LoginComponent,
     HomeComponent,
     CoursComponent,
-    CoursDetailsComponent
+    CoursDetailsComponent,
+    ElevesComponent,
+    AbsencesComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatMenuModule,
+
+],
+
   providers: [
     {
       provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptService, multi: true
