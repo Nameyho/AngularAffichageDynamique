3b70a5a1d8335eb93de995cd50c2bf8391d018d1
Nameyho <37048681+Nameyho@users.noreply.github.com>
2020-07-18 02:34:59 +0200

    Ajout-resultat.component

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/angular-heroku'));
+app.use(express.static(__dirname+'./dist/angular-heroku'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/AngularAffichageDynamique/'}),
+  res.sendFile(path.join(__dirname+'/dist/angular-heroku')),
 );
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
